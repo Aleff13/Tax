1dcba4764333162c47e22f2e98f09811c215a612
aleff menna resler <83561751+Aleff13@users.noreply.github.com>
2022-03-13 16:33:05 -0300

    Add twig template in project

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -6,11 +6,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Home{
 
-
     public function homepage(){
 
         return new Response('Hello');
 
     }
 
+    public function login(){
+
+        return new Response("Future page for using ");
+    }
 }
