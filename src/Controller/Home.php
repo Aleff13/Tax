1b84be6dfff526b8b0836f1568df72719e8993a2
aleff menna resler <83561751+Aleff13@users.noreply.github.com>
2022-03-13 17:03:00 -0300

    Now the template runs, and using twig variables

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class Home{
+class Home extends AbstractController{
 
     public function homepage(){
 
-        return new Response('Hello');
+        $labels = [
 
+            'Email',
+            'Password'
+
+        ];
+
+        return $this->render('homepage/homepage.html.twig', [
+            'title' => 'Estou vindo do controller',
+            'labels' => $labels
+        ]);
     }
 
     public function login(){
